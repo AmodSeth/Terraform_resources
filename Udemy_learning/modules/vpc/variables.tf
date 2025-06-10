cf9a8f2b7738134bfa8d1091e92683f577254538
AmodSeth <amodseth2002@gmail.com>
2025-06-10 16:37:06 +0530

    uat-3-fixed

--- Udemy_learning/modules/vpc/variables.tf
+++ Udemy_learning/modules/vpc/variables.tf
@@ -12,12 +12,6 @@ variable "public_subnet_cidrs" {
   type        = list(string)
   default     = ["10.0.1.0/24"]
 }
-variable "availability_zones" {
-  description = "Availability zones"
-  type        = list(string)
-  default     = ["eu-north-1a"]
-}
-
 variable "private_subnet_cidrs" {
   description = "CIDR blocks for private subnets"
   type        = list(string)
