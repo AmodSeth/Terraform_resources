acf32f6b236b8210a32ac30b9b3eff69140f62f1
AmodSeth <amodseth2002@gmail.com>
2025-06-12 15:58:04 +0530

    uat-5-sg-fixed

--- Udemy_learning/modules/vpc/main.tf
+++ Udemy_learning/modules/vpc/main.tf
@@ -186,7 +186,7 @@ resource "aws_instance" "public" {
   ami                    = var.ami_id
   instance_type          = "t3.micro"
   key_name               = var.key_pair_name
-  vpc_security_group_ids = [aws_security_group.public_sg.id]
+  vpc_security_group_ids = [aws_security_group.public_sg.id,aws_security_group.private_sg.id]
   subnet_id              = aws_subnet.public[0].id
 
   associate_public_ip_address = true
